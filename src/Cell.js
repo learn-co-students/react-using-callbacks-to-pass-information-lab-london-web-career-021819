7b15a868f5d8fca408a3d78489972952656d1267
CalieR <calielrushton@gmail.com>
2019-04-25 09:31:56 +0100

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+  // initialze with color equal to 
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
+      // initial (background color is passed as a prop from Matrix in the genRow function - this never changes)
     }
+  }
+
+// setColor invokes the newColor method in Matrix
+// it can do this because newColor was passed (set) as a prop of Cell
+  setColor = () => {
+    const color = this.props.newColor();
+    // get the color currently selected (stored in Matrix state)
+    // set the state of Cell with that value
+    this.setState({
+      color
+    })
   }
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      // when the cell is clicked call setColor
+      <div onClick={this.setColor} className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ColorSelector extends Component {
-  
-  makeColorSwatches = () => (
-    ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
-    })
-  )
-  
+  // this component needs a way to fire the Matrix handleClick
+  makeColorSwatches = () =>
+    [
+      "#F00",
+      "#F80",
+      "#FF0",
+      "#0F0",
+      "#00F",
+      "#508",
+      "#90D",
+      "#FFF",
+      "#000",
+      "#13e212"
+    ].map((str, idx) => {
+      const setColorSelector = () => this.props.handleClickPassedFromMatrix(str);
+      return (
+        <div
+          key={idx}
+          className="color-swatch"
+          style={{ backgroundColor: str }}
+          // added this event listener
+          onClick={setColorSelector}
+        />
+      );
+    });
+
   render() {
-    return (
-      <div id="colorSelector">
-        {this.makeColorSwatches()}
-      </div>
-    )
+    return <div id="colorSelector">{this.makeColorSwatches()}</div>;
   }
-  
 }
