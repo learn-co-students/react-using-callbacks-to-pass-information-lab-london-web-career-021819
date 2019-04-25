31647cae57717d7da0bcd2e89b58cc51bbd4af98
mariamjaludi <mariam.jaludi@gmail.com>
2019-04-25 10:32:52 +0100

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+//setColor method is passed from matrix as a prop.
+//onClick, the colorChanger method is called, which uses the str
+//from the map and calls setColor with it.
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const colorChanger = () => this.props.setColor(str)
+      return (<div
+        key={idx}
+        className="color-swatch"
+        onClick={colorChanger}
+        style={{backgroundColor: str}}
+        />)
     })
   )
-  
+
   render() {
     return (
-      <div id="colorSelector">
+      <div id="colorSelector" >
         {this.makeColorSwatches()}
       </div>
     )
   }
-  
+
 }
