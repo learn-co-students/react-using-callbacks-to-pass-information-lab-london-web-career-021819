5f1bb16e7c526c0739e442ecec5eea9c23f1fb9c
CalieR <calielrushton@gmail.com>
2019-04-25 09:37:24 +0100

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Cell extends Component {
-  // initialze with color equal to 
+  // initialze with color equal to
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       color: this.props.color
       // initial (background color is passed as a prop from Matrix in the genRow function - this never changes)
-    }
+    };
   }
 
-// setColor invokes the newColor method in Matrix
-// it can do this because newColor was passed (set) as a prop of Cell
+  // setColor invokes the newColor function in Matrix
+  // it can do this because newColor function was passed (set) as a prop of Cell
   setColor = () => {
     const color = this.props.newColor();
     // get the color currently selected (stored in Matrix state)
     // set the state of Cell with that value
     this.setState({
       color
-    })
-  }
-  
+    });
+  };
+
   render() {
     return (
       // when the cell is clicked call setColor
-      <div onClick={this.setColor} className="cell" style={{backgroundColor: this.state.color}}>
-      </div>
-    )
+      <div
+        onClick={this.setColor}
+        className="cell"
+        style={{ backgroundColor: this.state.color }}
+      />
+    );
   }
-  
 }
